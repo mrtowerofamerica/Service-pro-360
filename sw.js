67219797a2d8c4ecf751347b6209338e81423071
mrtowerofamerica <mrtowerofamerica1@gmail.com>
2025-08-10 17:36:21 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+// Service Pro 360 - simple offline cache
+const CACHE = 'sp360-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    try {
+      const net = await fetch(e.request);
+      (await caches.open(CACHE)).put(e.request, net.clone());
+      return net;
+    } catch {
+      return (await caches.match(e.request)) || Response.error();
+    }
+  })());
+});
